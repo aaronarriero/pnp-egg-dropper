4e11cc55a25cd131ae176eb3455f38ef20c67d1c
aaronarriero <aaronarriero@gmail.com>
2018-04-14 10:55:44 +0200

    Actualizado algoritmo de búsqueda binaria con PartialSolution.

--- src/EggDropper/Basic.php
+++ src/EggDropper/Basic.php
@@ -1,6 +1,7 @@
 <?php namespace Aaron\EggDropper;
 
 use Aaron\EggDropper\Lib;
+use Aaron\EggDropper\PartialSolution;
 
 class Basic
 {
@@ -19,47 +20,42 @@ class Basic
     {
         $minDrops100 = 0;
         for ($criticalFloor = 1; $criticalFloor <= 100; $criticalFloor++) {
-            $drops = self::exploreFloorsBinarySearch(1, 100, $criticalFloor);
-            $minDrops100 = max($minDrops100, $drops);
+            $ps = new PartialSolution(1, 100, $criticalFloor, 0, 100);
+            while (!$ps->isSolved()) {
+                $ps = self::solvePsBinarySearch($ps);
+            }
+            $minDrops100 = max($minDrops100, $ps->drops);
         }
         return $minDrops100;
     }
 
     /**
-     * Esta función determina el número mínimo de veces que es necesario lanzar un
-     * huevo para averiguar criticalFloor. Su funcionamiento es similar al de una
-     * búsqueda binaria, sin embargo como no conocemos criticalFloor, no podemos
-     * detener la búsqueda una vez que $currentFloor === $criticalFloor.
+     * Esta función resuelve una iteración del algoritmo de búsqueda binaria. Su
+     * funcionamiento consiste en lanzar un huevo desde la planta intermedia
+     * entre $minFloor y $maxFloor, sin embargo al no conocer criticalFloor, no
+     * podemos detener la búsqueda una vez que $currentFloor === $criticalFloor.
      *
-     * Es por esto que el algoritmo continúa la búsqueda acotando $minFloor y
-     * $maxFloor hasta que ambas son iguales. Llegado a este punto se ha explorado
-     * todo el espacio de soluciones y se devuelve el número de $drops o
-     * lanzamientos que han sido necesarios.
+     * Si el huevo se rompe exploramos pisos inferiores. No acotamos el piso
+     * superior ($currentFloor - 1) ya que de hacerlo el algoritmo nunca
+     * convergería.
      *
-     * Si $currentFloor > $criticalFloor el huevo se rompe, por lo que exploramos
-     * pisos inferiores. No acotamos el piso superior ($currentFloor - 1) ya que de
-     * hacerlo el algoritmo nunca convergería.
-     *
-     * Si $currentFloor <= $criticalFloor, el huevo no se rompe, por lo que
-     * exploramos pisos superiores. Esta vez sí es necesario acotar el piso inferior
-     * ($currentFloor + 1).
+     * Si el huevo no se rompe exploramos pisos superiores. Esta vez sí es
+     * necesario acotar el piso inferior ($currentFloor + 1).
      */
-    public static function exploreFloorsBinarySearch(int $minFloor, int $maxFloor, int $criticalFloor, int $drops = 0): int
+    public static function solvePsBinarySearch(PartialSolution $ps): PartialSolution
     {
-        // Ya se ha explorado todo el espacio de soluciones
-        if ($minFloor === $maxFloor) {
-            return $drops;
-        }
-
         // Determinamos desde qué piso lanzamos el huevo
-        $currentFloor = Lib::midpoint($minFloor, $maxFloor);
+        $currentFloor = Lib::midpoint($ps->minFloor, $ps->maxFloor);
 
-        if (!Lib::doesEggBreak($currentFloor, $criticalFloor)) {
+        $ps->drops++;
+        if (Lib::doesEggBreak($currentFloor, $ps->criticalFloor)) {
             // Acotamos la búsqueda descartando la mitad superior
-            return self::exploreFloorsBinarySearch($minFloor, $currentFloor, $criticalFloor, $drops + 1);
+            $ps->maxFloor = $currentFloor;
+            $ps->eggs--;
         } else {
-            // Acotamos la búsqueda descartando la mitad inferior
-            return self::exploreFloorsBinarySearch($currentFloor + 1, $maxFloor, $criticalFloor, $drops + 1);
+            // Acotamos la búsqueda descartando la mitad inferior más una planta
+            $ps->minFloor = $currentFloor + 1;
         }
+        return $ps;
     }
 }
