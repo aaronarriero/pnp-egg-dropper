b8dcbfb6e25c0d91c0c7112c33e13b257b8f0ad0
aaronarriero <aaronarriero@gmail.com>
2018-04-14 11:51:15 +0200

    Incluídos test de ejemplo para los distintos algoritmos.

--- tests/LibTest.php
+++ tests/LibTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Aaron\EggDropper\Lib;
+use Aaron\EggDropper\PartialSolution;
 use PHPUnit\Framework\TestCase;
 
 final class LibTest extends TestCase
@@ -22,4 +23,63 @@ final class LibTest extends TestCase
         $this->assertEquals(-15, Lib::midpoint(-10, -20));
     }
 
+    /**
+     * Caso de ejemplo que muestra la acotación realizada mediante el algoritmo
+     * de búsqueda binaria. $floors indica el [$minFloor, $maxFloor] tras cada
+     * iteración hasta que converge en el piso superior a $criticalFloor.
+     */
+    public function testSolvePsBinarySearch(): void
+    {
+        $floors = [
+            [51, 100],
+            [51, 75],
+            [51, 63],
+            [51, 57],
+            [51, 54],
+            [51, 52],
+            [51, 51],
+        ];
+        $index = 0;
+        $ps = new PartialSolution(1, 100, 50, 0, 100);
+        while (!$ps->isSolved()) {
+            $ps = Lib::solvePsBinarySearch($ps);
+            $this->assertEquals($floors[$index][0], $ps->minFloor);
+            $this->assertEquals($floors[$index][1], $ps->maxFloor);
+            $index++;
+        }
+    }
+
+    /**
+     * Caso de ejemplo que muestra el número necesario de lanzamientos mediante
+     * el algoritmo de búsqueda por saltos.
+     *
+     * @return void
+     */
+    public function testSolvePsJumpSearch(): void
+    {
+        $currentFloor = 14;
+        $floorsToClimb = 13;
+        $ps = new PartialSolution(1, 100, 100, 0, 2);
+        while (2 === $ps->eggs && !$ps->isSolved()) {
+            $ps = Lib::solvePsJumpSearch($ps, $currentFloor, $floorsToClimb);
+            $currentFloor = $currentFloor + $floorsToClimb;
+            $floorsToClimb--;
+        }
+        $this->assertEquals(12, $ps->drops);
+    }
+
+    /**
+     * Caso de ejemplo que muestra el número necesario de lanzamientos mediante
+     * el algoritmo de búsqueda secuencial.
+     *
+     * @return void
+     */
+    public function testSolvePsSequential(): void
+    {
+        $ps = new PartialSolution(1, 100, 100, 0, 1);
+        while (!$ps->isSolved()) {
+            $ps = Lib::solvePsSequential($ps);
+        }
+        $this->assertEquals(99, $ps->drops);
+    }
 }
